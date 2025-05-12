8677d6756dfd069805615df118d347a8119d71e9
lucas-raab-student <lurac3d@student.oaisd.org>
2025-05-12 10:42:43 -0400

    Added Stamina to my ui

--- Assets/Scripts/EnemyInstantKiil.cs
+++ Assets/Scripts/EnemyInstantKiil.cs
@@ -23,7 +23,7 @@ public class EnemyInstantKill : MonoBehaviour
                 animator.SetTrigger("Hit");
             }
 
-            playerHealth.TakeDamage(0);
+            playerHealth.TakeDamage(1);
         }
     }
 
--- Assets/Scripts/StaminaSystem.cs
+++ Assets/Scripts/StaminaSystem.cs
@@ -1,45 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StaminaSystem : MonoBehaviour
 {
+    [Header("Stamina Values")]
     public float maxStamina = 100f;
     public float currentStamina;
-    public float staminaRegenRate=10f;
+    public float staminaRegenRate = 10f;
     public float staminaRegenDelay = 2f;
-    public   float LastStaminaUseTime;
-    // Start is called before the first frame update
+    public float LastStaminaUseTime;
+
+    [Header("UI")]
+    public Slider staminaRegen; // Assign your slider in the inspector
+
     void Start()
     {
         currentStamina = maxStamina;
+
+        // Optional: Initialize the UI bar
+        if (staminaRegen != null)
+        {
+            staminaRegen.maxValue = maxStamina;
+            staminaRegen.value = currentStamina;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Regenerate stamina after delay
         if (Time.time > LastStaminaUseTime + staminaRegenDelay && currentStamina < maxStamina)
         {
             currentStamina += staminaRegenRate * Time.deltaTime;
-
             currentStamina = Mathf.Min(currentStamina, maxStamina);
         }
+
+        // Update UI bar
+        if (staminaRegen != null)
+        {
+            staminaRegen.value = currentStamina;
+        }
     }
+
     public bool UseStamina(float amount)
-    { 
-        if(currentStamina>=amount)
+    {
+        if (currentStamina >= amount)
         {
             currentStamina -= amount;
             LastStaminaUseTime = Time.time;
             return true;
-
         }
         return false;
     }
+
     public void RefiillStamona(float amount)
     {
         currentStamina += amount;
         currentStamina = Mathf.Min(currentStamina, maxStamina);
-        LastStaminaUseTime= Time.time;
+        LastStaminaUseTime = Time.time;
     }
 }
