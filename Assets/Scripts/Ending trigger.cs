1dcae0698a0945a0f4a6783564b2b8bb2223b20e
lucas-raab-student <lurac3d@student.oaisd.org>
2025-05-22 09:06:41 -0400

    Finished ending scene

---
+++ Assets/Scripts/Ending trigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Endingtrigger : MonoBehaviour
+{    public string triggerTag = "Player";
+
+    // Start is called before the first frame update
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(triggerTag))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
